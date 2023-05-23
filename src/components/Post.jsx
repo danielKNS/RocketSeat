2c8d5677f47cdb023ac3cda2ca8ce3df574b69b5
Daniel Ribeiro <danielrocha05lol@gmail.com>
2023-05-23 18:39:58 -0400

    fixed the structure of posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,19 +3,38 @@ import styles from "./Post.module.css";
 
 const Post = () => {
   return (
-    <article>
-      <header className={styles.post}>
+    <article className={styles.post}>
+      <header>
         <div className={styles.author}>
           <img
             className={styles.avatar}
             src="https://github.com/danielKNS.png"
           />
           <div className={styles.authorInfo}>
-            <strong></strong>
-            <span></span>
+            <strong> Diego Fernandes </strong>
+            <span> Web Developer </span>
           </div>
         </div>
+
+        <time title="11 of May at 8:30" dateTime="2023-05-11">
+          Published in 1h
+        </time>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare </a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
     </article>
   );
 };
